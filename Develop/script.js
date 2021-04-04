02572e5ed1a8db86f75ea3932fcc8dd3649a3644
AliMitchell <ali.mitchell.n@gmail.com>
2021-04-04 14:34:36 -0600

    updated with a var to hold total characters, vars to hold:special characters, uppercase letters, and numbers; a function to combine these based on confirm window and generate a password to display; functions starts on click and ends by displaying in the window.

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,48 @@
 
-
+var uppercaseLetters = ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+var characters = ['!','@','#','$','%','&','*']
+var numbers = ['1','2','3','4','5','6','7','8','9']
+var total = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-window.alert ('Indicate the parameters of your password.');
+function generatePassword () {
+  window.alert ('Indicate the parameters of your password.');
 
-console.log (alert)
+  var lengthConfirm = prompt ('How many characters would you like?');
 
-var A = window.confirm ('More than 8 Characters');
+  var passCase = confirm('Include Uppercase Letters?');
+    if(passCase) {
+      total = total.concat (uppercaseLetters) 
+    }
 
+    var passCharacter = confirm ('Include Special Characters?');
+    if(passCharacter) {
+      total = total.concat (characters) 
+    }
+
+    var passNumber = confirm ('Include a Number');
+    if(passNumber) {
+      total = total.concat (numbers)
+    }
+
+    console.log (total)
+    var password = []
+      for (var i=0; i<lengthConfirm; i++) {
+      var index = Math.floor (Math.random ()*total.length)
+      console.log (index)
+      console.log (total[index])
+      password.push(total[index])
+    }
+  
+    return password.join("")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log (password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
